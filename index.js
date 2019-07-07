7ad86affaaa4a472c7baf54a891ccdfe06831fc9
ankingcodes <ankush.bhardwaj0@gmail.com>
2019-07-06 21:44:04 -0400

    express server

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express') //this is required to involve express
+var app = express(); //The app returned by express() is in
+//fact a JavaScript Function, designed to be passed to Node’s
+//HTTP servers as a callback to handle requests.
+var bodyParser = require('body-parser');
+var todoRoutes = require('./routes/todos');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+var port = 3000; //can be any port
+//starts a UNIX socket and listens for connections on the given path
+//app.listen(port,host,backlog,callback) binds and listens for connections
+//on a specified host and port. If port is omitted, then the operating
+//system will automatically assign an arbitrary unused port.
+app.listen(port,function(){
+  console.log("app is running" + port);
+})
+//routes http get request to specified path '/' with callback
+//callbacks can be middle ware functions, series of middlewares,
+//array of middle ware or combination of all
+app.get('/',function(req,res){
+  res.send({message: "hello"}); //it actually calls res.json()
+});
+
+ app.use('/api/todos', todoRoutes);
